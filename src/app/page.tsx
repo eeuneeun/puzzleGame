ed902dcea9d6bc63a283f135e39b9085929e3542
euneun <sacroo@naver.com>
2025-03-19 15:04:19 +0900

    배열 생성 함수로 교체체

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,29 @@ export default function Main() {
     const nowTop: number = Number(targetEl.top.replace("px", ""));
     const nowLeft: number = Number(targetEl.left.replace("px", ""));
 
+    // # 상하좌우 키 눌렀을 경우 이동 함수
     if (event.key === "ArrowDown") {
-      if (nowTop !== 900) targetEl.top = nowTop + 100 + "px";
+      if (nowTop !== 800) targetEl.top = nowTop + 100 + "px";
     } else if (event.key === "ArrowUp") {
       if (nowTop !== 0) targetEl.top = nowTop - 100 + "px";
     } else if (event.key === "ArrowLeft") {
       if (nowLeft !== 0) targetEl.left = nowLeft - 100 + "px";
     } else if (event.key === "ArrowRight") {
-      if (nowLeft !== 900) targetEl.left = nowLeft + 100 + "px";
+      if (nowLeft !== 800) targetEl.left = nowLeft + 100 + "px";
+    }
+    // # 엔터 키 눌렀을 경우 블럭 이동 함수
+    if (event.key === "Enter") {
     }
   }
 
+  // # 임시 배열 함수
+  function arrLoop() {
+    const newArr = [];
+    for (let i = 0; i < 80; i++) {
+      newArr.push(<div key={i}>{i + 1}</div>);
+    }
+    return newArr;
+  }
   // useEffect(() => {
   //   return () => {
   //     console.log("useEffect");
@@ -30,87 +42,7 @@ export default function Main() {
         <div className="me">
           <input type="text" id="me" onKeyDown={(e) => keyboardMove(e)} />
         </div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
-        <div>11</div>
-        <div>12</div>
-        <div>13</div>
-        <div>14</div>
-        <div>15</div>
-        <div>16</div>
-        <div>17</div>
-        <div>18</div>
-        <div>19</div>
-        <div>20</div>
-        <div>21</div>
-        <div>22</div>
-        <div>23</div>
-        <div>24</div>
-        <div>25</div>
-        <div>26</div>
-        <div>27</div>
-        <div>28</div>
-        <div>29</div>
-        <div>30</div>
-        <div>31</div>
-        <div>32</div>
-        <div>33</div>
-        <div>34</div>
-        <div>35</div>
-        <div>36</div>
-        <div>37</div>
-        <div>38</div>
-        <div>39</div>
-        <div>40</div>
-        <div>41</div>
-        <div>42</div>
-        <div>43</div>
-        <div>44</div>
-        <div>45</div>
-        <div>46</div>
-        <div>47</div>
-        <div>48</div>
-        <div>49</div>
-        <div>50</div>
-        <div>51</div>
-        <div>52</div>
-        <div>53</div>
-        <div>54</div>
-        <div>55</div>
-        <div>56</div>
-        <div>57</div>
-        <div>58</div>
-        <div>59</div>
-        <div>60</div>
-        <div>61</div>
-        <div>62</div>
-        <div>63</div>
-        <div>64</div>
-        <div>65</div>
-        <div>66</div>
-        <div>67</div>
-        <div>68</div>
-        <div>69</div>
-        <div>70</div>
-        <div>71</div>
-        <div>72</div>
-        <div>73</div>
-        <div>74</div>
-        <div>75</div>
-        <div>76</div>
-        <div>77</div>
-        <div>78</div>
-        <div>79</div>
-        <div>80</div>
+        {arrLoop()}
       </main>
     </div>
   );
